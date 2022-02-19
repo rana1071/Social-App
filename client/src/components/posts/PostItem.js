85430b6650b1a49a500861ba76476724cc01c4b8
rana1071 <sarthakrana.cse19@chitkarauniversity.edu.in>
2022-02-19 13:46:43 +0530

    make it better

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -8,11 +8,11 @@ import {addLike, removeLike, deletePost} from '../../actions/post';
 
 const PostItem = ({deletePost,addLike,removeLike,auth, post: {_id, text, name, avatar, user, likes, comments,date}}) => {
   return (
-    <div class="post bg-white p-1 my-1">
+    <div className="post bg-white p-1 my-1">
           <div>
             <Link to={`/profile/${user}`}>
               <img
-                class="round-img"
+                className="round-img"
                 src={avatar}
                 alt=""
               />
@@ -20,30 +20,30 @@ const PostItem = ({deletePost,addLike,removeLike,auth, post: {_id, text, name, a
             </Link>
           </div>
           <div>
-            <p class="my-1">
+            <p className="my-1">
                 {text}
             </p>
-             <p class="post-date">
+             <p className="post-date">
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
-            <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
-              <i class="fas fa-thumbs-up"></i>
+            <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
+              <i className="fas fa-thumbs-up"></i>
               <span>{likes.length}</span>
             </button>
-            <button onClick={e => removeLike(_id)} type="button" class="btn btn-light">
-              <i class="fas fa-thumbs-down"></i>
+            <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
+              <i className="fas fa-thumbs-down"></i>
             </button>
           
-            {/*<Link to={`/post/${_id}`} className="btn btn-primary">
-              Discussion <span class='comment-count'>{comments.length}</span>
+            {/*<Link to={`/post/${_id}`} classNameName="btn btn-primary">
+              Discussion <span className='comment-count'>{comments.length}</span>
                </Link>*/}
             {!auth.loading && user===auth.user._id && (
            
                  <button  onClick={e => deletePost(_id)}    
                   type="button"
-                  class="btn btn-danger"
+                  className="btn btn-danger"
                 >
-                  <i class="fas fa-times"></i>
+                  <i className="fas fa-times"></i>
                 </button>
             )}
             
